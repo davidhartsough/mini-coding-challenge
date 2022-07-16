586a0d9a6ee7f751f18a657f4eefc9e16381535d
davidhartsough <hartsoughdavid@gmail.com>
2022-07-16 14:11:13 +1000

    improve comments and add JSDoc typedefs

--- src/filter-metadata.js
+++ src/filter-metadata.js
@@ -8,14 +8,27 @@ type Metadata = {
   author: string | null;
 };
 */
+/**
+ * @typedef Metadata
+ * @prop {string | null} url
+ * @prop {string | null} siteName
+ * @prop {string | null} title
+ * @prop {string | null} description
+ * @prop {string[] | null} keywords
+ * @prop {string | null} author
+ */
 
 /**
  * Filters the given Metadata array to only include the objects that match the given search query.
+ * Match on all properties of the Metadata objects.
  * If the search query has multiple words,
  * treat each word as a separate search term to filter by,
  * in addition to gathering results from the overall query.
- * If the search query has special characters,
+ * If the search query has special characters, including most punctuation,
  * run the query filter with the special characters removed.
+ * If the metadata contains special characters, including most punctuation,
+ * also run the query filter with those special characters removed.
+ * (Consider non-alphanumeric characters, such as hyphens, periods, commas, quotes, etc.)
  * Can return an empty array if no Metadata objects match the search query.
  * @param {Metadata[]} metadata - An array of Metadata objects
  * @param {string} query - The search query string
@@ -23,5 +36,6 @@ type Metadata = {
  */
 export default function filterMetadata(metadata, query) {
   // TODO: delete and replace this with your code
+  console.log(query);
   return metadata;
 }
--- src/get-metadata.js
+++ src/get-metadata.js
@@ -1,29 +1,42 @@
 // Note: Please do not use JSDOM or any other external library/package (sorry)
+// (Optional: Please consider creating a solution that could work with Node.js as well.)
 /*
 type Metadata = {
-  url: string;
-  siteName: string;
-  title: string;
-  description: string;
-  keywords: string[];
-  author: string;
+  url: string | null;
+  siteName: string | null;
+  title: string | null;
+  description: string | null;
+  keywords: string[] | null;
+  author: string | null;
 };
 */
+/**
+ * @typedef Metadata
+ * @prop {string | null} url
+ * @prop {string | null} siteName
+ * @prop {string | null} title
+ * @prop {string | null} description
+ * @prop {string[] | null} keywords
+ * @prop {string | null} author
+ */
 
 /**
- * Gets the URL, site name, title, description, keywords, and author info out of the <head> meta tags from a given html string.
+ * Gets the URL, site name, title, description, keywords, and author info
+ * out of the <head> meta tags from a given html string.
  * 1. Get the URL from the <meta property="og:url"> tag.
  * 2. Get the site name from the <meta property="og:site_name"> tag.
  * 3. Get the title from the the <title> tag.
  * 4. Get the description from the <meta property="og:description"> tag or the <meta name="description"> tag.
  * 5. Get the keywords from the <meta name="keywords"> tag and split them into an array.
  * 6. Get the author from the <meta name="author"> tag.
- * If any of the above tags are missing or if the values are empty, then the corresponding value will be null.
- * @param html The complete HTML document text to parse
- * @returns A Metadata object with data from the HTML <head>
+ * If any of the above tags are missing or if the values are empty,
+ * then the corresponding value will be null.
+ * @param {string} html The complete HTML document text to parse
+ * @returns {Metadata} A Metadata object with data from the HTML <head>
  */
 export default function getMetadata(html) {
   // TODO: delete and replace this with your code
+  console.log(html);
   return {
     url: "https://www.example.com/something/to-test",
     siteName: "Example Site Name",
